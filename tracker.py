aa70a8d5267d3ec3175bdf417759f918acdbd59c
zegheim <justinarista@gmail.com>
2019-01-10 14:50:59 +0000

    retained only the close info in json

--- tracker.py
+++ tracker.py
@@ -1,29 +1,41 @@
 import config
 import json
+import requests
 
 # Define constants
 API_KEY = config.API_KEY
 BASE_URL = 'https://www.worldtradingdata.com/api/v1/history'
 STOCKS = config.STOCKS
 
-with requests.Session() as s:
-    for ticker in STOCKS:
-        # Define a dictionary of parameters that we will pass to the API endpoint
-        payload = {
-            'symbol': ticker,
-            'sort': 'oldest',
-            'api_token': API_KEY,
-            'date_from': '2017-12-31',
-            'date_to': '2019-01-09',
-            }
+# with requests.Session() as s:
+#     for ticker in STOCKS:
+#         # Define a dictionary of parameters that we will pass to the API endpoint
+#         payload = {
+#             'symbol': ticker,
+#             'sort': 'oldest',
+#             'api_token': API_KEY,
+#             'date_from': '2018-01-01',
+#             'date_to': '2019-01-09',
+#             }
         
-        data = s.get(BASE_URL, params = payload)
+#         data = s.get(BASE_URL, params = payload)
 
-        with open(ticker + '.json', 'w') as f:
-            writer = csv.writer(f)
-            reader = csv.reader(data.text.splitlines())
+#         with open('json/' + ticker + '.json', 'w') as f:
+#             f.write(data.text)
 
-            for row in reader:
-                writer.writerow([row[0], row[2]])
+for ticker in STOCKS:
+    with open('json/' + ticker + '.json', 'r+') as f:
+        parsed_json = json.loads(f.read())
+        for key, val in parsed_json['history'].items():
+            val.pop('open')
+            val.pop('high')
+            val.pop('low')
+            val.pop('volume')
+        f.seek(0)
+        f.write(json.dumps(parsed_json))
+        f.truncate()
+
+
+
 
     
